7a98d44a12b22f8cddc9689f81370d84db624154
Raanan Weber <raananw@gmail.com@>
2012-04-10 21:38:03 +0200

    Short documentation

--- source/BasketGame.h
+++ source/BasketGame.h
@@ -1,3 +1,17 @@
+/*
+ * BasketBall Trainer
+ * This was my first OpenGL project.
+ * Was actually a university assignment to write an OpenGL Game with under 1200 lines of code
+ * (Not including Shapes and textures, of course).
+ * Was programmed during 2010.
+ * 
+ * I am "open sourcing" it, hoping someone can benefit from looking at the code.
+ * Would be great receiving feedback from anyone who learned something from it, or that has any questions.
+ *
+ * Raanan Weber, Berlin 2012, raananw "at" gmail.com
+*/
+
+
 #pragma once
 
 extern "C" {
